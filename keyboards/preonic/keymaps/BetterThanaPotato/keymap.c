90c45af51492702c4f72e3bbbce8db3151f38839
BetterThanaPotato <32472430+BetterThanaPotato@users.noreply.github.com>
2024-08-03 11:36:53 -0700

    KC_SYRQ doesnt seem to be keycode 99 in xev changed right sift on left space bar to KC_PSCR (print screen) temporarily

--- keyboards/preonic/keymaps/BetterThanaPotato/keymap.c
+++ keyboards/preonic/keymaps/BetterThanaPotato/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  NUMPAD,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_RSFT, KC_SPC,  RAISE,   KC_RALT, KC_PAUS, KC_BRK,  KC_SYRQ
+  NUMPAD,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_PSCR, KC_SPC,  RAISE,   KC_RALT, KC_PAUS, KC_BRK,  KC_SYRQ
 ),
 
 /* ,-----------------------------------------------------------------------------------.
